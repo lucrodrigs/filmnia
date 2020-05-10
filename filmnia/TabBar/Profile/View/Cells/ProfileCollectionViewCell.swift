b3901e36812144684c131bde94f877c351a631b8
lucrodrigs <lucrodrigs@gmail.com>
2020-05-10 15:02:43 -0300

    commit

---
+++ filmnia/TabBar/Profile/View/Cells/ProfileCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileCollectionViewCell.swift
+//  filmnia
+//
+//  Created by UserTQI on 10/05/20.
+//  Copyright © 2020 lucrodrigs. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
