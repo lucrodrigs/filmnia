8babf09ac0a5352830ce1b2835a388329d804e4d
lucrodrigs <lucrodrigs@gmail.com>
2020-05-25 16:46:29 -0300

    commit

---
+++ filmnia/Lists/Yourlist/View/Cell/YourlistCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  YourlistCollectionViewCell.swift
+//  filmnia
+//
+//  Created by UserTQI on 25/05/20.
+//  Copyright © 2020 lucrodrigs. All rights reserved.
+//
+
+import UIKit
+
+class YourlistCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
